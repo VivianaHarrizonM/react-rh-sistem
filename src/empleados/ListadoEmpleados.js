87c145323fad0668e6499cb8ec71affd12c3ebc6
Viviana Harrizon <viviana.harrizon@gmail.com>
2025-03-26 12:46:40 -0600

    creation of the Application to manage employees.

---
+++ src/empleados/ListadoEmpleados.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { NumericFormat } from 'react-number-format';
+
+export default function ListadoEmpleados() {
+
+  const urlBase= "http://localhost:8080/rh-app/empleados";
+  const[empleados, setEmpleados] = useState([]);
+
+  useEffect(() => {
+    cargarEmpleados();
+  },[]);
+
+  const cargarEmpleados = async () => {
+    const resultado = await axios.get(urlBase);
+    console.log("Resultado cargar empleados");
+    console.log(resultado.data);
+    setEmpleados(resultado.data);
+  }
+  return (
+    <div className='container' >
+      <div className='container text-center' style={{margin: "30px"}}>
+      </div>
+    
+      <table className="table table-striped table-hover align-middle" style={{border:"1px solid gray"}}>
+      <thead className="table-primary" style={{border:"1.8px solid gray"}}>
+        <tr >
+          <th scope="col">Id</th>
+          <th scope="col">Empleado</th>
+          <th scope="col">Departamento</th>
+          <th scope="col">Sueldo</th>
+        </tr>
+      </thead>
+      <tbody>
+      {
+         //Iteramos el arreglo de empleados 
+        empleados.map((empleado, indice)=> (
+        <tr key={indice}>
+          <th scope="row">{empleado.idEmpleado}</th>
+          <td>{empleado.nombre}</td>
+          <td>{empleado.departamento}</td>
+          <td>
+            <NumericFormat value={empleado.sueldo}
+              displayType={'text'}
+              thousandSeparator=','
+              prefix={'$'}
+              decimalScale={2}
+              fixedDecimalScale/>
+          </td>
+        </tr>
+        ))
+      }
+      </tbody>
+    </table>
+        </div>
+      )
+    }
